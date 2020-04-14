b16667edf43c04a8106b4794ace6642607f74f9f
shafie <shafiemukhre@gmail.com>
2020-04-14 10:33:55 -0700

    update

--- src/components/Insights.js
+++ src/components/Insights.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Typography, Card, List } from '@material-ui/core';
+import { Grid, Typography, } from '@material-ui/core';
 import { makeStyles }  from '@material-ui/core/styles';
 import { grey } from '@material-ui/core/colors';
 import InsightsList from './InsightsList';
